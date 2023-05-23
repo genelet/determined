e1f8573c43c65489a6e2600c6b4dfbb9d6a0f548
Peter Bi <genelet@gmail.com>
2023-05-23 09:01:20 -0400

    bk

--- deth/common.go
+++ deth/common.go
@@ -11,14 +11,14 @@ import (
 //	║ Go type          │ Conversion        │ Label ║
 //	╠══════════════════╪═══════════════════╪═══════╣
 //	║ string           │ ending Struct     │  no   ║
-//	║ [2]string        │ ending Struct     │  1    ║
-//	║ [3]string        │ ending Struct     │  2    ║
-//	║ [4]string        │ ending Struct     │  3    ║
-//	║ [5]string        │ ending Struct     │  4    ║
-//	║ [6]string        │ ending Struct     │  5    ║
-//	║ [7]string        │ ending Struct     │  6    ║
-//	║ [8]string        │ ending Struct     │  7    ║
-//	║                  │                   │       ║
+//	║ [1]string        │ ending Struct     │  1    ║
+//	║ [2]string        │ ending Struct     │  2    ║
+//	║ [3]string        │ ending Struct     │  3    ║
+//	║ [4]string        │ ending Struct     │  4    ║
+//	║ [5]string        │ ending Struct     │  5    ║
+//	║ [6]string        │ ending Struct     │  6    ║
+//	║ [7]string        │ ending Struct     │  7    ║
+//  ║                  │                   │       ║
 //	║ [2]interface{}   │ SingleStruct      │  no   ║
 //	║ [3]interface{}   │ SingleStruct      │  1    ║
 //	║ [4]interface{}   │ SingleStruct      │  2    ║
@@ -29,13 +29,13 @@ import (
 //	║ [9]interface{}   │ SingleStruct      │  7    ║
 //  ║                  │                   │       ║
 //	║ []string         │ ending ListStruct │  no   ║
-//	║ [][2]string      │ ending ListStruct │  1    ║
-//	║ [][3]string      │ ending ListStruct │  2    ║
-//	║ [][4]string      │ ending ListStruct │  3    ║
-//	║ [][5]string      │ ending ListStruct │  4    ║
-//	║ [][6]string      │ ending ListStruct │  5    ║
-//	║ [][7]string      │ ending ListStruct │  6    ║
-//	║ [][8]string      │ ending ListStruct │  7    ║
+//	║ [][1]string      │ ending ListStruct │  1    ║
+//	║ [][2]string      │ ending ListStruct │  2    ║
+//	║ [][3]string      │ ending ListStruct │  3    ║
+//	║ [][4]string      │ ending ListStruct │  4    ║
+//	║ [][5]string      │ ending ListStruct │  5    ║
+//	║ [][6]string      │ ending ListStruct │  6    ║
+//	║ [][7]string      │ ending ListStruct │  7    ║
 //  ║                  │                   │       ║
 //	║ [][2]interface{} │ ListStruct        │  no   ║
 //	║ [][3]interface{} │ ListStruct        │  1    ║
@@ -51,260 +51,200 @@ import (
 //	╚══════════════════╧═══════════════════════════╝
 func NewValue(v interface{}) (*Value, error) {
 	switch t := v.(type) {
-
 	// string is treated as ending Struct without fields
 	case string:
-		v, err := newSingleStruct([]interface{}{t}, 2)
+		v, err := newSingleStruct([2]interface{}{t}, 0)
 		if err != nil { return nil, err }
 		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
-	case [2]interface{}:
-		var x []interface{}
-		for _, s := range t { x = append(x, s) }
-		v, err := newSingleStruct(x, 2)
+	case [1]string:
+		v, err := newSingleStruct([2]interface{}{t[0]}, 1)
 		if err != nil { return nil, err }
 		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
 	case [2]string:
-		var x []interface{}
-		for _, s := range t { x = append(x, s) }
-		v, err := newSingleStruct(x, 3)
-		if err != nil { return nil, err }
-		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
-	case [3]interface{}:
-		var x []interface{}
-		for _, s := range t { x = append(x, s) }
-		v, err := newSingleStruct(x, 3)
+		v, err := newSingleStruct([2]interface{}{t[0]}, 2)
 		if err != nil { return nil, err }
 		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
 	case [3]string:
-		var x []interface{}
-		for _, s := range t { x = append(x, s) }
-		v, err := newSingleStruct(x, 4)
-		if err != nil { return nil, err }
-		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
-	case [4]interface{}:
-		var x []interface{}
-		for _, s := range t { x = append(x, s) }
-		v, err := newSingleStruct(x, 4)
+		v, err := newSingleStruct([2]interface{}{t[0]}, 3)
 		if err != nil { return nil, err }
 		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
 	case [4]string:
-		var x []interface{}
-		for _, s := range t { x = append(x, s) }
-		v, err := newSingleStruct(x, 5)
-		if err != nil { return nil, err }
-		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
-	case [5]interface{}:
-		var x []interface{}
-		for _, s := range t { x = append(x, s) }
-		v, err := newSingleStruct(x, 5)
+		v, err := newSingleStruct([2]interface{}{t[0]}, 4)
 		if err != nil { return nil, err }
 		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
 	case [5]string:
-		var x []interface{}
-		for _, s := range t { x = append(x, s) }
-		v, err := newSingleStruct(x, 6)
-		if err != nil { return nil, err }
-		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
-	case [6]interface{}:
-		var x []interface{}
-		for _, s := range t { x = append(x, s) }
-		v, err := newSingleStruct(x, 6)
+		v, err := newSingleStruct([2]interface{}{t[0]}, 5)
 		if err != nil { return nil, err }
 		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
 	case [6]string:
-		var x []interface{}
-		for _, s := range t { x = append(x, s) }
-		v, err := newSingleStruct(x, 7)
-		if err != nil { return nil, err }
-		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
-	case [7]interface{}:
-		var x []interface{}
-		for _, s := range t { x = append(x, s) }
-		v, err := newSingleStruct(x, 7)
+		v, err := newSingleStruct([2]interface{}{t[0]}, 6)
 		if err != nil { return nil, err }
 		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
 	case [7]string:
-		var x []interface{}
-		for _, s := range t { x = append(x, s) }
-		v, err := newSingleStruct(x, 8)
+		v, err := newSingleStruct([2]interface{}{t[0]}, 7)
+		if err != nil { return nil, err }
+		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
+
+	case [2]interface{}:
+		v, err := newSingleStruct([2]interface{}{t[0], t[1]}, 0)
+		if err != nil { return nil, err }
+		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
+	case [3]interface{}:
+		v, err := newSingleStruct([2]interface{}{t[0], t[1]}, 1)
+		if err != nil { return nil, err }
+		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
+	case [4]interface{}:
+		v, err := newSingleStruct([2]interface{}{t[0], t[1]}, 2)
+		if err != nil { return nil, err }
+		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
+	case [5]interface{}:
+		v, err := newSingleStruct([2]interface{}{t[0], t[1]}, 3)
+		if err != nil { return nil, err }
+		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
+	case [6]interface{}:
+		v, err := newSingleStruct([2]interface{}{t[0], t[1]}, 4)
+		if err != nil { return nil, err }
+		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
+	case [7]interface{}:
+		v, err := newSingleStruct([2]interface{}{t[0], t[1]}, 5)
 		if err != nil { return nil, err }
 		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
 	case [8]interface{}:
-		var x []interface{}
-		for _, s := range t { x = append(x, s) }
-		v, err := newSingleStruct(x, 8)
-		if err != nil { return nil, err }
-		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
-	case [8]string:
-		var x []interface{}
-		for _, s := range t { x = append(x, s) }
-		v, err := newSingleStruct(x, 9)
+		v, err := newSingleStruct([2]interface{}{t[0], t[1]}, 6)
 		if err != nil { return nil, err }
 		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
 	case [9]interface{}:
-		var x []interface{}
-		for _, s := range t { x = append(x, s) }
-		v, err := newSingleStruct(x, 9)
+		v, err := newSingleStruct([2]interface{}{t[0], t[1]}, 7)
 		if err != nil { return nil, err }
 		return &Value{Kind: &Value_SingleStruct{SingleStruct: v}}, nil
 
 	case []string:
-		var x [][]interface{}
+		var x [][2]interface{}
 		for _, u := range t {
-			y := []interface{}{u}
-			x = append(x, y)
+			x = append(x, [2]interface{}{u})
 		}
-		v, err := newListStruct(x, 2)
+		v, err := newListStruct(x, 0)
 		if err != nil { return nil, err }
         return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
-	case [][2]interface{}:
-		var x [][]interface{}
+	case [][1]string:
+		var x [][2]interface{}
 		for _, u := range t {
-			var y []interface{}
-			for _, s := range u { y = append(y, s) }
-			x = append(x, y)
+			x = append(x, [2]interface{}{u[0]})
 		}
-		v, err := newListStruct(x, 2)
+		v, err := newListStruct(x, 1)
 		if err != nil { return nil, err }
         return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
 	case [][2]string:
-		var x [][]interface{}
+		var x [][2]interface{}
 		for _, u := range t {
-			var y []interface{}
-			for _, s := range u { y = append(y, s) }
-			x = append(x, y)
+			x = append(x, [2]interface{}{u[0]})
 		}
-		v, err := newListStruct(x, 3)
-		if err != nil { return nil, err }
-        return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
-	case [][3]interface{}:
-		var x [][]interface{}
-		for _, u := range t {
-			var y []interface{}
-			for _, s := range u { y = append(y, s) }
-			x = append(x, y)
-		}
-		v, err := newListStruct(x, 3)
+		v, err := newListStruct(x, 2)
 		if err != nil { return nil, err }
         return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
 	case [][3]string:
-		var x [][]interface{}
+		var x [][2]interface{}
 		for _, u := range t {
-			var y []interface{}
-			for _, s := range u { y = append(y, s) }
-			x = append(x, y)
+			x = append(x, [2]interface{}{u[0]})
 		}
-		v, err := newListStruct(x, 4)
-		if err != nil { return nil, err }
-        return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
-	case [][4]interface{}:
-		var x [][]interface{}
-		for _, u := range t {
-			var y []interface{}
-			for _, s := range u { y = append(y, s) }
-			x = append(x, y)
-		}
-		v, err := newListStruct(x, 4)
+		v, err := newListStruct(x, 3)
 		if err != nil { return nil, err }
         return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
 	case [][4]string:
-		var x [][]interface{}
+		var x [][2]interface{}
 		for _, u := range t {
-			var y []interface{}
-			for _, s := range u { y = append(y, s) }
-			x = append(x, y)
+			x = append(x, [2]interface{}{u[0]})
 		}
-		v, err := newListStruct(x, 5)
-		if err != nil { return nil, err }
-        return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
-	case [][5]interface{}:
-		var x [][]interface{}
-		for _, u := range t {
-			var y []interface{}
-			for _, s := range u { y = append(y, s) }
-			x = append(x, y)
-		}
-		v, err := newListStruct(x, 5)
+		v, err := newListStruct(x, 4)
 		if err != nil { return nil, err }
         return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
 	case [][5]string:
-		var x [][]interface{}
+		var x [][2]interface{}
 		for _, u := range t {
-			var y []interface{}
-			for _, s := range u { y = append(y, s) }
-			x = append(x, y)
+			x = append(x, [2]interface{}{u[0]})
 		}
-		v, err := newListStruct(x, 6)
-		if err != nil { return nil, err }
-        return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
-	case [][6]interface{}:
-		var x [][]interface{}
-		for _, u := range t {
-			var y []interface{}
-			for _, s := range u { y = append(y, s) }
-			x = append(x, y)
-		}
-		v, err := newListStruct(x, 6)
+		v, err := newListStruct(x, 5)
 		if err != nil { return nil, err }
         return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
 	case [][6]string:
-		var x [][]interface{}
+		var x [][2]interface{}
 		for _, u := range t {
-			var y []interface{}
-			for _, s := range u { y = append(y, s) }
-			x = append(x, y)
+			x = append(x, [2]interface{}{u[0]})
 		}
-		v, err := newListStruct(x, 7)
-		if err != nil { return nil, err }
-        return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
-	case [][7]interface{}:
-		var x [][]interface{}
-		for _, u := range t {
-			var y []interface{}
-			for _, s := range u { y = append(y, s) }
-			x = append(x, y)
-		}
-		v, err := newListStruct(x, 7)
+		v, err := newListStruct(x, 6)
 		if err != nil { return nil, err }
         return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
 	case [][7]string:
-		var x [][]interface{}
+		var x [][2]interface{}
 		for _, u := range t {
-			var y []interface{}
-			for _, s := range u { y = append(y, s) }
-			x = append(x, y)
+			x = append(x, [2]interface{}{u[0]})
 		}
-		v, err := newListStruct(x, 8)
+		v, err := newListStruct(x, 7)
+		if err != nil { return nil, err }
+        return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
+
+	case [][2]interface{}:
+		var x [][2]interface{}
+		for _, u := range t {
+			x = append(x, [2]interface{}{u[0], u[1]})
+		}
+		v, err := newListStruct(x, 0)
+		if err != nil { return nil, err }
+        return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
+	case [][3]interface{}:
+		var x [][2]interface{}
+		for _, u := range t {
+			x = append(x, [2]interface{}{u[0], u[1]})
+		}
+		v, err := newListStruct(x, 1)
+		if err != nil { return nil, err }
+        return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
+	case [][4]interface{}:
+		var x [][2]interface{}
+		for _, u := range t {
+			x = append(x, [2]interface{}{u[0], u[1]})
+		}
+		v, err := newListStruct(x, 2)
+		if err != nil { return nil, err }
+        return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
+	case [][5]interface{}:
+		var x [][2]interface{}
+		for _, u := range t {
+			x = append(x, [2]interface{}{u[0], u[1]})
+		}
+		v, err := newListStruct(x, 3)
+		if err != nil { return nil, err }
+        return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
+	case [][6]interface{}:
+		var x [][2]interface{}
+		for _, u := range t {
+			x = append(x, [2]interface{}{u[0], u[1]})
+		}
+		v, err := newListStruct(x, 4)
+		if err != nil { return nil, err }
+        return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
+	case [][7]interface{}:
+		var x [][2]interface{}
+		for _, u := range t {
+			x = append(x, [2]interface{}{u[0], u[1]})
+		}
+		v, err := newListStruct(x, 5)
 		if err != nil { return nil, err }
         return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
 	case [][8]interface{}:
-		var x [][]interface{}
+		var x [][2]interface{}
 		for _, u := range t {
-			var y []interface{}
-			for _, s := range u { y = append(y, s) }
-			x = append(x, y)
+			x = append(x, [2]interface{}{u[0], u[1]})
 		}
-		v, err := newListStruct(x, 8)
-		if err != nil { return nil, err }
-        return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
-	case [][8]string:
-		var x [][]interface{}
-		for _, u := range t {
-			var y []interface{}
-			for _, s := range u { y = append(y, s) }
-			x = append(x, y)
-		}
-		v, err := newListStruct(x, 9)
+		v, err := newListStruct(x, 6)
 		if err != nil { return nil, err }
         return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
 	case [][9]interface{}:
-		var x [][]interface{}
+		var x [][2]interface{}
 		for _, u := range t {
-			var y []interface{}
-			for _, s := range u { y = append(y, s) }
-			x = append(x, y)
+			x = append(x, [2]interface{}{u[0], u[1]})
 		}
-		v, err := newListStruct(x, 9)
+		v, err := newListStruct(x, 7)
 		if err != nil { return nil, err }
         return &Value{Kind: &Value_ListStruct{ListStruct: v}}, nil
 
@@ -319,59 +259,41 @@ func NewValue(v interface{}) (*Value, error) {
 }
 
 // NewStruct constructs a Struct from a generic Go map.
-// v... should be string labels, up to 7.
-// Plus optionally map[string]interface{}.
+// v is optionally map[string]interface{}.
 // The map values are converted using NewValue.
-func NewStruct(name string, v ...interface{}) (*Struct, error) {
-	x := &Struct{ClassName: name}
+func NewStruct(name string, v ...map[string]interface{}) (*Struct, error) {
+	return newStruct(name, 0, v...)
+}
+
+func newStruct(name string, n int, v ...map[string]interface{}) (*Struct, error) {
+	x := &Struct{ClassName: name, NLabels: int32(n)}
 	if v == nil { return x, nil }
 
-TOP:
-	for i, value := range v {
-		switch t := value.(type) {
-		case string:
-			switch i {
-			case 0: x.Label1 = t
-			case 1: x.Label2 = t
-			case 2: x.Label3 = t
-			case 3: x.Label4 = t
-			case 4: x.Label5 = t
-			case 5: x.Label6 = t
-			case 6: x.Label7 = t
-			default:
-			}
-		case map[string]interface{}:
-			x.Fields = make(map[string]*Value, len(t))
-			for key, val := range t {
-				if !utf8.ValidString(key) {
-					return nil, protoimpl.X.NewError("invalid UTF-8 in key: %q", key)
-				}
-				var err error
-				x.Fields[key], err = NewValue(val)
-				if err != nil { return nil, err }
-			}
-			break TOP
-		default:
+	x.Fields = make(map[string]*Value)
+	for key, val := range v[0] {
+		if !utf8.ValidString(key) {
+			return nil, protoimpl.X.NewError("invalid UTF-8 in key: %q", key)
 		}
+		var err error
+		x.Fields[key], err = NewValue(val)
+		if err != nil { return nil, err }
 	}
 	return x, nil
 }
 
-func newSingleStruct(v []interface{}, n int) (*Struct, error) {
-	m := len(v)
-	if (n!=m && (n-1)!=m) || m<1 {
-		return nil, protoimpl.X.NewError("wrong counts: m %d => n %d", m, n)
-	}
-
+func newSingleStruct(v [2]interface{}, n int) (*Struct, error) {
 	name, ok := v[0].(string)
 	if !ok {
 		return nil, protoimpl.X.NewError("need string %T for class name", v[0])
 	}
+	if v[1] == nil {
+		return newStruct(name, n)
+	}
 
-	return NewStruct(name, v[1:]...)
+	return newStruct(name, n, v[1].(map[string]interface{}))
 }
 
-func newListStruct(v [][]interface{}, n int) (*ListStruct, error) {
+func newListStruct(v [][2]interface{}, n int) (*ListStruct, error) {
 	var err error
 	x := make([]*Struct, len(v))
 	for i, u := range v {
--- deth/common_test.go
+++ deth/common_test.go
@@ -2,97 +2,79 @@ package deth
 
 import (
 	"testing"
+	"strings"
 )
 
 func TestCommonString(t *testing.T) {
 	endpoint, err := NewStruct(
-		"Geo", "label1_1", "label1_2", map[string]interface{}{
-			"TheString888": "Circle",
-			"TheString777": [2]string{"Circle", "label2_1"},
-			"TheString":    [2]interface{}{"Circle"},
-			"TheString666": [3]interface{}{"Circle", "label2_2"},
+		"Geo", map[string]interface{}{
+			"TheString0": "Circle",
+			"TheString1": [1]string{"Circle"},
+			"TheString2": [2]string{"Circle"},
 
-			"TheList888":   []string{"CircleClass1", "CircleClass2"},
-			"TheList": [][2]interface{}{
-				{"CircleClass1"},
-				{"CircleClass2"}},
-
-			"TheHash": [][2]string{
-				{"CircleClass1", "a1"},
-				{"CircleClass2", "a2"}},
-			"TheHash888": [][3]interface{}{
-				{"CircleClass1", "a1"},
-				{"CircleClass2", "a2"}},
+			"TheList0":   []string{"CircleClass1", "CircleClass2"},
+			"TheList1":   [][1]string{{"CircleClass1"}, {"CircleClass2"}},
+			"TheList2":   [][2]string{{"CircleClass1"}, {"CircleClass2"}},
 		},
 	)
 	if err != nil {
 		panic(err)
 	}
-		t.Errorf("%s", endpoint.String())
-
 	fields := endpoint.GetFields()
-	if fields["TheString888"].String() != fields["TheString"].String() {
-		t.Errorf("%s", fields["TheString888"].String())
-		t.Errorf("%s", fields["TheString"].String())
+	if strings.ReplaceAll(fields["TheString0"].String()," ", "") != "single_struct:{className:\"Circle\"}" {
+		t.Errorf("%#v", fields["TheString0"].String())
 	}
-	if fields["TheList888"].String() != fields["TheList"].String() {
-		t.Errorf("%s", fields["TheList888"].String())
-		t.Errorf("%s", fields["TheList"].String())
+	if strings.ReplaceAll(fields["TheString1"].String()," ", "") != "single_struct:{className:\"Circle\"nLabels:1}" {
+		t.Errorf("%#v", fields["TheString1"].String())
 	}
-	if fields["TheMap888"].String() != fields["TheMap"].String() {
-		t.Errorf("%s", fields["TheMap888"].String())
-		t.Errorf("%s", fields["TheMap"].String())
+	if strings.ReplaceAll(fields["TheString2"].String()," ", "") != "single_struct:{className:\"Circle\"nLabels:2}" {
+		t.Errorf("%#v", fields["TheString2"].String())
+	}
+	if strings.ReplaceAll(fields["TheList0"].String()," ", "") != "list_struct:{list_fields:{className:\"CircleClass1\"}list_fields:{className:\"CircleClass2\"}}" {
+		t.Errorf("%#v", fields["TheList0"].String())
+	}
+	if strings.ReplaceAll(fields["TheList1"].String()," ", "") != "list_struct:{list_fields:{className:\"CircleClass1\"nLabels:1}list_fields:{className:\"CircleClass2\"nLabels:1}}" {
+		t.Errorf("%#v", fields["TheList1"].String())
+	}
+	if strings.ReplaceAll(fields["TheList2"].String()," ", "") != "list_struct:{list_fields:{className:\"CircleClass1\"nLabels:2}list_fields:{className:\"CircleClass2\"nLabels:2}}" {
+		t.Errorf("%#v", fields["TheList2"].String())
 	}
 }
 
 func TestCommonStruct(t *testing.T) {
 	endpoint, err := NewStruct(
 		"Geo", map[string]interface{}{
-			"Shape": [2]interface{}{
-				"Class1", map[string]interface{}{"Field1": "Circle"}},
+			"Shape1": [2]interface{}{
+				"Class1", map[string]interface{}{"Field1": "Circle1"}},
+			"Shape2": [3]interface{}{
+				"Class2", map[string]interface{}{"Field2": [2]string{"Circle2"}}},
 		},
 	)
 	if err != nil {
 		panic(err)
 	}
 	shapeFields := endpoint.GetFields()
-	shapeEndpoint := shapeFields["Shape"].GetSingleStruct()
+
+	shapeEndpoint := shapeFields["Shape1"].GetSingleStruct()
 	field1Fields := shapeEndpoint.GetFields()
 	field1Endpoint := field1Fields["Field1"].GetSingleStruct()
 	if endpoint.ClassName != "Geo" ||
 		shapeEndpoint.ClassName != "Class1" ||
-		field1Endpoint.ClassName != "Circle" {
+		field1Endpoint.ClassName != "Circle1" {
 		t.Errorf("shape endpoint: %s", shapeEndpoint.String())
 		t.Errorf("field 1 endpoint: %s", field1Endpoint.String())
 	}
-}
 
-/*
-func TestCommonMap(t *testing.T) {
-	endpoint, err := NewStruct(
-		"Geo", map[string]interface{}{
-			"HashShapes": map[string][2]interface{}{
-				"x1": [2]interface{}{
-					"Class5", map[string]interface{}{"Field4": "Circle"}},
-				"y1": [2]interface{}{
-					"Class6", map[string]interface{}{"Field5": "Circle"}}},
-		},
-	)
-	if err != nil {
-		panic(err)
-	}
-	shapeFields := endpoint.GetFields()
-	shapeEndpoint := shapeFields["HashShapes"].GetListStruct().GetListFields()
-	field1Fields := shapeEndpoint.GetFields()
-	field1Endpoint := field1Fields["Field4"].GetSingleStruct()
-	if endpoint.ClassName != "Geo" ||
-		shapeEndpoint.ClassName != "Class5" ||
-		field1Endpoint.ClassName != "Circle" {
-		t.Errorf("shape endpoint: %s", shapeEndpoint.String())
-		t.Errorf("field 1 endpoint: %s", field1Endpoint.String())
+	shape2Endpoint := shapeFields["Shape2"].GetSingleStruct()
+	field2Fields := shape2Endpoint.GetFields()
+	field2Endpoint := field2Fields["Field2"].GetSingleStruct()
+	if endpoint.ClassName != "Geo" || endpoint.NLabels != 0 ||
+		shape2Endpoint.ClassName != "Class2" || shape2Endpoint.NLabels != 1 ||
+		field2Endpoint.ClassName != "Circle2" || field2Endpoint.NLabels != 2 {
+		t.Errorf("shape endpoint: %s", shape2Endpoint.String())
+		t.Errorf("field 2 endpoint: %s", field2Endpoint.String())
 	}
 }
-*/
 
 func TestCommonList(t *testing.T) {
 	endpoint, err := NewStruct(
