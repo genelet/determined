0415064cf1f90aefeef30ddea7aedbce2e4bdcfa
Peter Bi <genelet@gmail.com>
2023-05-31 20:35:54 -0400

    loopFields

--- det/clone.go
+++ det/clone.go
@@ -1,7 +1,9 @@
 package det
 
 import (
+	"encoding/json"
 	"reflect"
+	"unicode"
 )
 
 // clone clones a value via pointer
@@ -17,4 +19,50 @@ func clone(old interface{}) interface{} {
 	}
 
 	return obj.Interface()
+}
+
+func loopFields(t reflect.Type, objectMap map[string]*Value) ([]reflect.StructField, map[string]reflect.StructField, error) {
+	var newFields []reflect.StructField
+	origTypes := make(map[string]reflect.StructField)
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		name := field.Name
+		if !unicode.IsUpper([]rune(name)[0]) {
+			continue
+		}
+		tcontent := field.Tag.Get("json")
+		if tcontent == `-` || (len(tcontent)>=2 && tcontent[len(tcontent)-2:] == `,-`) {
+			continue
+		}
+		if tcontent == "" {
+			deeps, deepTypes, err := loopFields(field.Type, objectMap)
+			if err != nil { return nil, nil, err }
+			for _, v := range deeps {
+				newFields = append(newFields, v)
+			}
+			for k, v := range deepTypes {
+				origTypes[k] = v
+			}
+			continue
+		}
+		if result, ok := objectMap[name]; ok {
+			newField := reflect.StructField{Name: name, Tag: field.Tag}
+			if result.GetMapStruct() != nil {
+				newField.Type = reflect.TypeOf(map[string]json.RawMessage{})
+			} else if result.GetListStruct() != nil {
+				if field.Type.Kind() == reflect.Map {
+					newField.Type = reflect.TypeOf(map[string]json.RawMessage{})
+				} else {
+					newField.Type = reflect.TypeOf([]json.RawMessage{})
+				}
+			} else {
+				newField.Type = reflect.TypeOf(json.RawMessage{})
+			}
+			newFields = append(newFields, newField)
+			origTypes[name] = field
+		} else {
+			newFields = append(newFields, field)
+		}
+	}
+	return newFields, origTypes, nil
 }
--- det/unmarshal.go
+++ det/unmarshal.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	"reflect"
-	"unicode"
 )
 
 // JsonUnmarshal unmarshals JSON data with interfaces determined by Determined.
@@ -24,157 +23,132 @@ func JsonUnmarshal(dat []byte, current interface{}, spec *Struct, ref map[string
 	}
 
 	t := reflect.TypeOf(current).Elem()
-	oriValue := reflect.ValueOf(&current).Elem()
-	tmp := reflect.New(oriValue.Elem().Type()).Elem()
-	tmp.Set(oriValue.Elem())
-
-	n := t.NumField()
-
-	var newFields []reflect.StructField
-	found := false
-	for i := 0; i < n; i++ {
-		field := t.Field(i)
-		name := field.Name
-		if unicode.IsUpper([]rune(name)[0]) && field.Tag == "" {
-			return fmt.Errorf("missing tag for %s", name)
-		}
-		if result, ok := objectMap[name]; ok {
-			newField := reflect.StructField{Name: name, Tag: field.Tag}
-			if result.GetMapStruct() != nil {
-				newField.Type = reflect.TypeOf(map[string]json.RawMessage{})
-			} else if result.GetListStruct() != nil {
-				if field.Type.Kind() == reflect.Map {
-					newField.Type = reflect.TypeOf(map[string]json.RawMessage{})
-				} else {
-					newField.Type = reflect.TypeOf([]json.RawMessage{})
-				}
-			} else {
-				newField.Type = reflect.TypeOf(json.RawMessage{})
-			}
-			newFields = append(newFields, newField)
-			found = true
-		} else {
-			newFields = append(newFields, field)
-		}
-	}
-	if found == false {
-		return json.Unmarshal(dat, current)
+	newFields, origTypes, err := loopFields(t, objectMap)	
+	if err != nil {
+		return err
 	}
 
 	newType := reflect.StructOf(newFields)
 	raw := reflect.New(newType).Interface()
-	err := json.Unmarshal(dat, raw)
+	err = json.Unmarshal(dat, raw)
 	if err != nil {
 		return err
 	}
 	rawValue := reflect.ValueOf(raw).Elem()
 
-	for i := 0; i < n; i++ {
-		field := t.Field(i)
-		fieldType := field.Type
-		name := field.Name
-		f := tmp.Elem().Field(i)
+	oriValue := reflect.ValueOf(&current).Elem()
+	tmp := reflect.New(oriValue.Elem().Type()).Elem()
+	tmp.Set(oriValue.Elem())
+
+	for i, field2 := range newFields {
 		rawField := rawValue.Field(i)
+		name := field2.Name
+		field, ok := origTypes[name]
+		if !ok {
+			field = field2
+		}
+		fieldType := field.Type
+		f := tmp.Elem().FieldByName(name)
 		result, ok := objectMap[name]
-		if ok {
-			if x := result.GetMapStruct(); x != nil {
-				nextMapStructs := x.GetMapFields()
-				nSmaller := len(nextMapStructs)
-				if nSmaller == 0 {
-					return fmt.Errorf("missing map struct for %s", name)
-				}
-				var first *Struct
-				for _, first = range nextMapStructs {
-					break
-				}
+		if !ok {
+			f.Set(rawField)
+			continue
+		}
+		if x := result.GetMapStruct(); x != nil {
+			nextMapStructs := x.GetMapFields()
+			nSmaller := len(nextMapStructs)
+			if nSmaller == 0 {
+				return fmt.Errorf("missing map struct for %s", name)
+			}
+			var first *Struct
+			for _, first = range nextMapStructs {
+				break
+			}
 
-				n := rawField.Len()
-				keys := rawField.MapKeys()
-				fMap := reflect.MakeMap(fieldType)
-				for i := 0; i < n; i++ {
-					key := keys[i]
-					v := rawField.MapIndex(key)
-					nextStruct := first
-					if i < nSmaller {
-						if tmp, ok := nextMapStructs[key.String()]; ok {
-							nextStruct = tmp
-						}
-					}
-					trial := clone(ref[nextStruct.ClassName])
-					err := JsonUnmarshal(v.Bytes(), trial, nextStruct, ref)
-					if err != nil {
-						return err
-					}
-					fMap.SetMapIndex(key, reflect.ValueOf(trial))
-				}
-				f.Set(fMap)
-			} else if x := result.GetListStruct(); x != nil {
-				n := rawField.Len()
-
-				nextListStructs := x.GetListFields()
-				nSmaller := len(nextListStructs)
-				if nSmaller == 0 {
-					return fmt.Errorf("missing list struct for %s", field.Name)
-				}
-				first := nextListStructs[0]
-
-				var fSlice, fMap reflect.Value
-				var keys []reflect.Value
-				if fieldType.Kind() == reflect.Map {
-					keys = rawField.MapKeys()
-					fMap = reflect.MakeMapWithSize(fieldType, n)
-				} else {
-					fSlice = reflect.MakeSlice(fieldType, n, n)
-				}
-
-				for k := 0; k < n; k++ {
-					var key, v reflect.Value
-					if fieldType.Kind() == reflect.Map {
-						key = keys[k]
-						v = rawField.MapIndex(key)
-					} else {
-						v = rawField.Index(k)
-					}
-
-					nextStruct := first
-					if k < nSmaller {
-						nextStruct = nextListStructs[k]
-					}
-
-					trial := clone(ref[nextStruct.ClassName])
-					err := JsonUnmarshal(v.Bytes(), trial, nextStruct, ref)
-					if err != nil {
-						return err
-					}
-					if fieldType.Kind() == reflect.Map {
-						fMap.SetMapIndex(key, reflect.ValueOf(trial))
-					} else {
-						fSlice.Index(k).Set(reflect.ValueOf(trial))
+			n := rawField.Len()
+			keys := rawField.MapKeys()
+			fMap := reflect.MakeMap(fieldType)
+			for i := 0; i < n; i++ {
+				key := keys[i]
+				v := rawField.MapIndex(key)
+				nextStruct := first
+				if i < nSmaller {
+					if tmp, ok := nextMapStructs[key.String()]; ok {
+						nextStruct = tmp
 					}
 				}
-				if fieldType.Kind() == reflect.Map {
-					f.Set(fMap)
-				} else {
-					f.Set(fSlice)
-				}
-			} else if x := result.GetSingleStruct(); x != nil {
-				trial := ref[x.ClassName]
-				if trial == nil {
-					return protoimpl.X.NewError("class ref not found for %s", x.ClassName)
-				}
-				trial = clone(trial)
-				err := JsonUnmarshal(rawField.Bytes(), trial, x, ref)
+				trial := clone(ref[nextStruct.ClassName])
+				err := JsonUnmarshal(v.Bytes(), trial, nextStruct, ref)
 				if err != nil {
 					return err
 				}
-				if f.Kind() == reflect.Interface || f.Kind() == reflect.Ptr {
-					f.Set(reflect.ValueOf(trial))
+				fMap.SetMapIndex(key, reflect.ValueOf(trial))
+			}
+			f.Set(fMap)
+		} else if x := result.GetListStruct(); x != nil {
+			n := rawField.Len()
+
+			nextListStructs := x.GetListFields()
+			nSmaller := len(nextListStructs)
+			if nSmaller == 0 {
+				return fmt.Errorf("missing list struct for %s", field.Name)
+			}
+			first := nextListStructs[0]
+
+			var fSlice, fMap reflect.Value
+			var keys []reflect.Value
+			if fieldType.Kind() == reflect.Map {
+				keys = rawField.MapKeys()
+				fMap = reflect.MakeMapWithSize(fieldType, n)
+			} else {
+				fSlice = reflect.MakeSlice(fieldType, n, n)
+			}
+
+			for k := 0; k < n; k++ {
+				var key, v reflect.Value
+				if fieldType.Kind() == reflect.Map {
+					key = keys[k]
+					v = rawField.MapIndex(key)
 				} else {
-					f.Set(reflect.ValueOf(trial).Elem())
+					v = rawField.Index(k)
+				}
+
+				nextStruct := first
+				if k < nSmaller {
+					nextStruct = nextListStructs[k]
+				}
+
+				trial := clone(ref[nextStruct.ClassName])
+				err := JsonUnmarshal(v.Bytes(), trial, nextStruct, ref)
+				if err != nil {
+					return err
+				}
+				if fieldType.Kind() == reflect.Map {
+					fMap.SetMapIndex(key, reflect.ValueOf(trial))
+				} else {
+					fSlice.Index(k).Set(reflect.ValueOf(trial))
 				}
 			}
-		} else if unicode.IsUpper([]rune(field.Name)[0]) {
-			f.Set(rawField)
+			if fieldType.Kind() == reflect.Map {
+				f.Set(fMap)
+			} else {
+				f.Set(fSlice)
+			}
+		} else if x := result.GetSingleStruct(); x != nil {
+			trial := ref[x.ClassName]
+			if trial == nil {
+				return protoimpl.X.NewError("class ref not found for %s", x.ClassName)
+			}
+			trial = clone(trial)
+			err := JsonUnmarshal(rawField.Bytes(), trial, x, ref)
+			if err != nil {
+				return err
+			}
+			if f.Kind() == reflect.Interface || f.Kind() == reflect.Ptr {
+				f.Set(reflect.ValueOf(trial))
+			} else {
+				f.Set(reflect.ValueOf(trial).Elem())
+			}
 		}
 	}
 
